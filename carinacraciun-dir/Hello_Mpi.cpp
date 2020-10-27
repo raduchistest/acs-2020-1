47a2a2037e8b3759ef66b67d5dc5522cb504d2b9
IronLady05 <geo.craciun92@gmail.com>
2020-10-27 09:44:16 +0200

    Second commit with some changed in the file Hello_Mpi.cpp

--- carinacraciun-dir/Hello_Mpi.cpp
+++ carinacraciun-dir/Hello_Mpi.cpp
@@ -1,14 +1,22 @@
-#include "mpi.h"
-#include <stdio.h>
 
-int main( int argc, char *argv[] )
-{
-    int rank, size;
+#include <mpi.h>
 
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-    printf( "Hello World from process %d of %d\n", rank, size );
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    printf("Hello world! from rank %d"
+           " out of %d processors\n",
+           world_rank, world_size);
+
+    // Finalize the MPI environment.
     MPI_Finalize();
-    return 0;
 }
